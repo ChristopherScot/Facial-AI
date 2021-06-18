d89badee998ef60baafc113588fc6ed4b48ca14d
Mr. Martin <martincchs@gmail.com>
2021-06-17 21:47:56 -0400

    test

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from flask import Flask, redirect
 from FER.main import fer
 import secrets
 
+
 app = Flask(__name__)
 app.register_blueprint(fer, url_prefix='/FER')
 app.secret_key = secrets.token_urlsafe(32)
